8c34b27c0fa3f0c739f4f467777ce600f47f51e9
jaredpar <jaredpar@microsoft.com>
2015-02-20 17:32:59 -0800

    Can compile Microsoft.CodeAnalysis.Desktop

--- nix/build.sh
+++ nix/build.sh
@@ -6,10 +6,15 @@ SYS_IMM_PATH=~/Documents/roslyn/packages/System.Collections.Immutable.1.1.33-bet
 SYS_MD_PATH=~/Documents/roslyn/packages/System.Reflection.Metadata.1.0.18-beta/lib/portable-net45+win8/System.Reflection.Metadata.dll
 
 pushd ~/Documents/roslyn
-pushd src/Compilers/Core/Portable
 
+pushd src/Compilers/Core/Portable
 mono $CSC_TOOL @/Users/jaredpar/Documents/roslyn/nix/MS.CA.rsp
 popd
+
+pushd src/Compilers/Core/Desktop
+mono $CSC_TOOL @/Users/jaredpar/Documents/roslyn/nix/MS.CA.Desktop.rsp
+popd
+
 #pushd src/Tools/Source/FakeSign
 
 #mono $CSC_TOOL Program.cs /parallel- /target:exe /unsafe+ /r:$REF_PATH"System.IO.dll" /r:$REF_PATH"System.Runtime.dll" /r:$SYS_IMM_PATH /r:$SYS_MD_PATH /out:$OUT_PATH"FakeSign.exe"
