edbfcdec5fef7de5a30312e41bae33c21982dc6c
Jared Parsons <jaredpar@microsoft.com>
2017-08-29 17:42:08 -0700

    demo

---
+++ src/Test/Utilities/Portable/FixBuild.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Roslyn.Test.Utilities
+{
+    public sealed class FixBuild
+    {
+        public IncrementalHash GetIncrementalHash() => throw new NotSupportedException();
+    }
+}
