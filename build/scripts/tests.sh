399593c5b92ee01bbcc6af49ec4fd74f11098986
Jared Parsons <jaredpparsons@gmail.com>
2018-09-26 07:08:45 -0700

    Setup the repro

--- build/scripts/tests.sh
+++ build/scripts/tests.sh
@@ -25,7 +25,7 @@ if [[ "${runtime}" == "dotnet" ]]; then
     xunit_console="${nuget_dir}"/xunit.runner.console/"${xunit_console_version}"/tools/netcoreapp2.0/xunit.console.dll
 elif [[ "${runtime}" == "mono" ]]; then
     file_list=(
-        "${unittest_dir}/Microsoft.CodeAnalysis.CSharp.Semantic.UnitTests/net46/Microsoft.CodeAnalysis.CSharp.Semantic.UnitTests.dll"
+        "${unittest_dir}/Microsoft.CodeAnalysis.CSharp.Emit.UnitTests/net46/Microsoft.CodeAnalysis.CSharp.Emit.UnitTests.dll"
         )
     xunit_console="${nuget_dir}"/xunit.runner.console/"${xunit_console_version}"/tools/net452/xunit.console.exe
 else
