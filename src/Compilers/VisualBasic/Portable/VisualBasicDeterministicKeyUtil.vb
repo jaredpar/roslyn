97fde401dc3eb9097ee59afb16583107c0334d8e
Jared Parsons <jaredpparsons@gmail.com>
2018-11-12 16:16:57 -0800

    Visual basic initial support

---
+++ src/Compilers/VisualBasic/Portable/VisualBasicDeterministicKeyUtil.vb
@@ -0,0 +1,32 @@
+﻿' Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+Imports System.Collections.Immutable
+Imports System.Collections.ObjectModel
+Imports System.ComponentModel
+Imports System.IO
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+Imports System.Text
+Imports System.Threading
+Imports Microsoft.CodeAnalysis.Operations
+Imports Microsoft.CodeAnalysis.Syntax
+Imports Microsoft.CodeAnalysis.VisualBasic
+Imports Microsoft.CodeAnalysis.VisualBasic.Symbols
+Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
+
+Namespace Microsoft.CodeAnalysis.VisualBasic
+    Friend Class VisualBasicDeterministicKeyUtil
+        Inherits DeterministicKeyUtil
+
+        Friend Shared Function GenerateKey(compilation As VisualBasicCompilation) As String
+            ' PROTOTYPE: must append all vb specific compilation options
+            Dim builder As New StringBuilder()
+            AppendCommonCompilationOptions(builder, compilation.Options)
+            AppendSyntaxTrees(builder, compilation.SyntaxTrees)
+            AppendReferences(builder, compilation.References)
+            Return builder.ToString()
+        End Function
+
+    End Class
+End Namespace
+
