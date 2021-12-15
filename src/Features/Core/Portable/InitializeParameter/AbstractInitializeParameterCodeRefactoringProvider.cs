853dc0e45bcbf08090c520440c2f66689ec97de2
Jared Parsons <jaredpparsons@gmail.com>
2021-12-14 16:28:03 -0800

    Null annotate ISymbol.ContainingSymbol

--- src/Features/Core/Portable/InitializeParameter/AbstractInitializeParameterCodeRefactoringProvider.cs
+++ src/Features/Core/Portable/InitializeParameter/AbstractInitializeParameterCodeRefactoringProvider.cs
@@ -16,6 +16,7 @@ using Microsoft.CodeAnalysis.Operations;
 using Microsoft.CodeAnalysis.PooledObjects;
 using Microsoft.CodeAnalysis.Shared.Extensions;
 using Microsoft.CodeAnalysis.Text;
+using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.InitializeParameter
 {
@@ -93,6 +94,7 @@ namespace Microsoft.CodeAnalysis.InitializeParameter
                 return;
             }
 
+            RoslynDebug.Assert(parameter.ContainingSymbol is not null);
             var methodSymbol = (IMethodSymbol)parameter.ContainingSymbol;
             if (methodSymbol.IsAbstract ||
                 methodSymbol.IsExtern ||
